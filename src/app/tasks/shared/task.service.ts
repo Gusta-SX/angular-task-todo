0ee5d4ae3fbd1c20194e802fecef6272fdad17d4
Gusta-SX <gugaa24@gmail.com>
2022-05-10 00:34:43 -0300

    build

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -6,7 +6,16 @@ import { Task } from '../shared/task';
   providedIn: 'root'
 })
 export class TaskService {
-  tasks: Task[] = []
+  tasks: Task[] = [
+    {id: 1, name: 'Task 1', description: 'essa é a task1', completed: false},
+    {id: 2, name: 'Task 2', description: 'essa é a task2', completed: false},
+    {id: 3, name: 'Task 3', description: 'essa é a task3', completed: false},
+    {id: 4, name: 'Task 4', description: 'essa é a task4', completed: false},
+    {id: 5, name: 'Task 5', description: 'essa é a task5', completed: false},
+    {id: 6, name: 'Task 6', description: 'essa é a task6', completed: false},
+    {id: 7, name: 'Task 7', description: 'essa é a task7', completed: false},
+    {id: 8, name: 'Task 8', description: 'essa é a task8', completed: false},
+  ]
 
   constructor() { }
 
@@ -29,9 +38,10 @@ export class TaskService {
     } else {
       let lastId = 0;
       if(this.tasks.length > 0) {
-        this.tasks[this.tasks.length].id;
+        this.tasks[this.tasks.length -1].id;
+      } else {
+        task.id = lastId + 1;
       }
-      task.id = lastId + 1;
       task.completed = false;
       this.tasks.push(task)
     }
